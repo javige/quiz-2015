275312766b907c33dc2032c8369fd80913635eb0
javige <javier.gestevez@iesfernandoaguilar.es>
2015-08-03 20:37:09 +0200

    Corregido error tipográfico en la validación de datos en quiz_controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -86,9 +86,7 @@ exports.create = function(req, res) {
   }
   var quiz = models.Quiz.build( req.body.quiz );
 
-  quiz
-  .validate()
-  .then(
+  quiz.validate().then(
     function(err){
       if (err) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
@@ -117,9 +115,7 @@ exports.update = function(req, res) {
   req.quiz.respuesta = req.body.quiz.respuesta;
   req.quiz.category = req.body.quiz.category;
   
-  req.quiz
-  .validate()
-  .then(
+  req.quiz.validate().then(
     function(err){
       if (err) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
